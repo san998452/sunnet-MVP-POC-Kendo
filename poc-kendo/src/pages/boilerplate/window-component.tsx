c2732b90db1f7741eb0e75a0070cd124b42fe514
santosh.yadav <santosh.yadav@anblicks.com>
2021-03-26 15:59:16 +0530

    Added window component

--- poc-kendo/src/pages/boilerplate/window-component.tsx
+++ poc-kendo/src/pages/boilerplate/window-component.tsx
@@ -1,35 +1,24 @@
 import React, { useState } from 'react';
 import { Window } from '@progress/kendo-react-dialogs';
+import Formcomponent from './form-component';
 const windowcomponent = () =>
 {
-    const [visible, setVisible] = useState(true);
+    const [visible, setVisible] = useState(false);
     const toggleDialog = () => {
         setVisible(!visible);
     };
     return(
         <section>
         <button className="k-button" onClick={toggleDialog}>Open Window</button>
-         {/* <Window title={"Status"} onClose={toggleDialog} initialHeight={350}>
-                            <form className="k-form">
-                                <fieldset>
-                                    <legend>User Details</legend>
-
-                                    <label className="k-form-field">
-                                        <span>First Name</span>
-                                        <input className="k-textbox" placeholder="Your Name" />
-                                    </label>
-                                    <label className="k-form-field">
-                                        <span>Last Name</span>
-                                        <input className="k-textbox" placeholder="Your Last Name" />
-                                    </label>
-                                </fieldset>
-
-                                <div className="text-right">
-                                    <button type="button" className="k-button" onClick={toggleDialog}>Cancel</button>
-                                    <button type="button" className="k-button k-primary" onClick={toggleDialog}>Submit</button>
-                                </div>
-                            </form>
-                    </Window>  */}
+            {visible && <Window 
+            title={"Status"} 
+            onClose={toggleDialog} 
+            initialHeight={350}
+            width={600}
+            >
+                <Formcomponent />
+            </Window>
+}
 
         </section>
     );
